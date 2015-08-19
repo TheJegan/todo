11ab89be9b33c437d46f548cb71405126969e058
jegan_matthews <matthewsjegan@gmail.com>
2015-08-19 10:14:08 -0700

    task view redesign

--- public/js/view/readTask.js
+++ public/js/view/readTask.js
@@ -4,7 +4,7 @@ var app = app || {};
 {
 	app.ReadTask = Backbone.View.extend({
 		el: '#todo-body',
-		template: Handlebars.compile( $('#list-template').html() ),
+		template: Handlebars.compile( $('#task-template').html() ),
 		events: {
 			'click .checkbox': 'showMenu'
 		},
@@ -23,93 +23,14 @@ var app = app || {};
 		render: function()
 		{
 			var task = this.model.where({_list: this.options.listId});
-			
-
 			var list = {
-							id: this.options.listId, 
-							tasks: _.map( task, function( model ){ return model.toJSON(); } )
-						};
+					id: this.options.listId, 
+					tasks: _.map( task, function( model ){ return model.toJSON(); } )
+				};
+
 			$(this.el).html(
 				this.template(list)
 			);
-
-
-			var self =this;
-			$('.menuItem').unbind('click').click(function(e)
-			{
-				// self.moveToList(e);
-				var listId = $(this).attr('data-id');
-				var taskId = $('input:checked').attr('data-id');
-
-				 self.moveToList(listId, taskId);
-			});
-
-			$('.delete').unbind('click').click(function(e)
-			{
-				e.preventDefault();
-				e.stopPropagation();
-
-				//simplify
-				self.deleteTodo( 
-					$(this).closest('.checkbox').find('input').attr('data-id'),
-					$(this).next('.taskName').text().trim()		
-					);
-			});
-			return this;
-		},
-		moveToList: function(listId, taskId)
-		{
-			var self = this;
-			var task = this.model.get(taskId);
-			task.set({_list: listId});		
-			task.save(null,
-			{
-				success: function() 
-				{
-					self.model.fetch({reset: true});
-		        }
-		    });
-
-		},
-		deleteTodo: function(id, taskName)
-		{
-			var self = this;
-
-			$('#myModal')
-			.find('.modal-body')
-			.html('<p> Are you sure you want to delete <strong>' + taskName + '</strong>?</p>');
-
-			$('#myModal').modal({
-			  keyboard: false
-			});
-
-
-			$('#yes').unbind('click').click(function(e)
-			{
-				$('#myModal').modal('hide')
-				self.DeleteTask(id);
-				// console.log(id);
-			});
-		},
-		DeleteTask: function(id)
-		{
-			var task = this.model.get(id);
-			task.destroy();
-		},
-		showMenu: function(e)
-		{
-			var HeightOfEachMenuItem = 50;
-
-	    	var menuHeight = $('.menuItem').length * HeightOfEachMenuItem;
-			var $menu = $('footer');
-	
-			$menu.animate({
-		        height: menuHeight
-		    }, 350);
-
-		},
-		close: function() {
-		   clearInterval(this.timer);
 		}
 	});
 })(jQuery);
