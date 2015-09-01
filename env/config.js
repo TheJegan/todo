0f3ec30ff5e973535fabc157a70f7d2563ec6571
jegan_matthews <matthewsjegan@gmail.com>
2015-08-31 23:15:17 -0700

    update heroku server urls

--- env/config.js
+++ env/config.js
@@ -1,16 +1,13 @@
 var twitterStrategy = {
 	consumerKey: "15k4F26hVvP0F6vjvDCVkw",
 	consumerSecret: "MGdqxBUI0lLoLc7KZYnW0xRNPAfpUL9diWFLU559lA",
-	callback: 'http://localhost:3000/auth/twitter/callback'
-	// callback: 'https://infinite-fjord-1986.herokuapp.com/auth/twitter/callback'
-
+	callback: 'https://infinite-fjord-1986.herokuapp.com/auth/twitter/callback'
 }
   
-// "http://192.168.0.3:3000/auth/twitter/callback"
+
 var config = {
 	twitter: twitterStrategy,
-	mongooseURL: 'mongodb://localhost/todo',
-	// mongooseURL: 'mongodb://jegan:todo@ds035563.mongolab.com:35563/todo',
+	mongooseURL: 'mongodb://jegan:todo@ds035563.mongolab.com:35563/todo',
 	isAuthenticated: function(req, res, next)
 	{
 		 if (req.isAuthenticated()) { 
