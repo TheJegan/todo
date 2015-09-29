5cfdfcdcd75339321a2bac1958cdedf979347e60
jegan_matthews <matthewsjegan@gmail.com>
2015-09-29 11:53:18 -0700

    Add superagent for testing passport
    (forgot to save supertest)

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
-var assert = require("assert");
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(2));
-    });
-  });
+var should = require('chai').should(),
+expect = require('chai').expect,
+supertest = require('supertest'),
+api = supertest('http://localhost:3000');
+
+// var assert = require("assert");
+
+describe('User', function(){
+	it('should return a 200 response', function(done)){
+		api.get('/users/1')
+		.set('Accept', 'application/json')
+		.expect(200, done)
+	}
 });
