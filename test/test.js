8c22fca706f8a569b26dfb99a225a96f2055750e
jegan_matthews <matthewsjegan@gmail.com>
2015-09-29 11:58:50 -0700

    installed superagent-oauth

--- test/test.js
+++ test/test.js
@@ -1,13 +1,16 @@
 var should = require('chai').should(),
 expect = require('chai').expect,
 supertest = require('supertest'),
+request = require('superagent');
 api = supertest('http://localhost:3000');
 
 // var assert = require("assert");
+require('superagent-oauth')(request);
 
 describe('User', function(){
 	it('should return a 200 response', function(done)){
 		api.get('/users/1')
+		.send({ user: 'hunter@hunterloftis.com', password: 'password' })
 		.set('Accept', 'application/json')
 		.expect(200, done)
 	}
